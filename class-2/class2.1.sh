544f0cabe0af5d213a65df50e583820378a3ffef
YBO <yitzhak851@gmail.com>
2024-04-24 16:25:50 +0300

    adding class02 dir

---
+++ class-2/class2.1.sh
@@ -0,0 +1,7 @@
+# class assignment 2.1
+mkdir classes # create directory `classes` in your home dir
+chmod a+rx . classes # make sure that everyone can read classes, as well as your home directory
+mkdir classes/class02 ; cd classes/class02  # create subdirectory named class 02 in classes and enter it
+cp /share/classes/class02/* .  # copy all files in ./class02into the current dir
+
+# ls -ld => show the files contain
