904742f67e248d23aefa153c6b3c1a6f8814544b
YBO <yitzhak851@gmail.com>
2024-06-08 22:32:50 +0300

    adding ex5 task 1.1, 1.2 , 1.3, 1.4

--- ex5/numList.c
+++ ex5/numList.c
@@ -108,6 +108,37 @@ double performOperation(const char* numList, char op);
 int isValidNumList(const char* str) {
    /***      !!!            DO NOT DEFINE ADDITIONAL LOCAL VARIABLES               !!!   ***/
    /***      Apply all changes to the code below this line. DO NOT DELETE THIS COMMENT   ***/
+    // Skip leading whitespace
+    while (*str != '\0' && (*str == ' ' || *str == '\t' || *str == '\n' || *str == '\v' || *str == '\f' || *str == '\r')) {
+        str++;
+    }
+    // Check for empty string after skipping whitespaces
+    if (*str == '\0') {
+        return 1;
+    }
+    // Process the string
+    while (*str != '\0') {
+        // Check for a valid number
+        if (!(*str >= '0' && *str <= '9')) {
+            return 0;
+        }
+        // Skip digits
+        while (*str != '\0' && (*str >= '0' && *str <= '9')) {
+            str++;
+        }
+        // Skip trailing whitespace after a number
+        while (*str != '\0' && (*str == ' ' || *str == '\t' || *str == '\n' || *str == '\v' || *str == '\f' || *str == '\r')) {
+            str++;
+        }
+        // If we reached the end of the string, it's a valid list
+        if (*str == '\0') {
+            return 1;
+        }
+        // If the next character is not a digit, return 0
+        if (!(*str >= '0' && *str <= '9')) {
+            return 0;
+        }
+    }
    /***      Apply all changes to the code above this line. DO NOT DELETE THIS COMMENT   ***/
    /*** should reach here iff str is a valid number list ***/
    return 1;
@@ -124,6 +155,26 @@ int numWordsInList(const char* str) {
    int numWords = 0;
    /***      !!!   DO NOT DEFINE ADDITIONAL LOCAL VARIABLES (OTHER THAN numWords)  !!!   ***/
    /***      Apply all changes to the code below this line. DO NOT DELETE THIS COMMENT   ***/
+    // Skip leading whitespace
+    while (*str != '\0' && (*str == ' ' || *str == '\t' || *str == '\n' || *str == '\v' || *str == '\f' || *str == '\r')) {
+        str++;
+    }
+    // Process the string
+    while (*str != '\0') {
+        // Check if we're at the start of a word
+        if (*str != ' ' && *str != '\t' && *str != '\n' && *str != '\v' && *str != '\f' && *str != '\r') {
+            numWords++;
+            // Skip the rest of the word
+            while (*str != '\0' && (*str != ' ' && *str != '\t' && *str != '\n' && *str != '\v' && *str != '\f' && *str != '\r')) {
+                str++;
+            }
+        }
+        // Skip trailing whitespace before the next word
+        while (*str != '\0' && (*str == ' ' || *str == '\t' || *str == '\n' || *str == '\v' || *str == '\f' || *str == '\r')) {
+            str++;
+        }
+    }
+
    /***      Apply all changes to the code above this line. DO NOT DELETE THIS COMMENT   ***/
    return numWords;
 }
@@ -142,6 +193,28 @@ double get1stNumberValue(const char* numList) {
    double value = 0.0;
    /***      !!!   DO NOT DEFINE ADDITIONAL LOCAL VARIABLES (OTHER THAN value)     !!!   ***/
    /***      Apply all changes to the code below this line. DO NOT DELETE THIS COMMENT   ***/
+    // Skip leading whitespace
+    while (*numList != '\0' && (*numList == ' ' || *numList == '\t' || *numList == '\n' || *numList == '\v' || *numList == '\f' || *numList == '\r')) {
+        numList++;
+    }
+    // Check if the string is empty after skipping whitespaces
+    if (*numList == '\0') {
+        return -2.0;
+    }
+    // Check if the first character is a digit
+    if (*numList < '0' || *numList > '9') {
+        return -1.0;
+    }
+    // Convert the first word to a number
+    while (*numList != '\0' && (*numList >= '0' && *numList <= '9')) {
+        value = value * 10 + (*numList - '0');
+        numList++;
+    }
+    // Check if there are any non-digit characters in the first word
+    if (*numList != '\0' && (*numList != ' ' && *numList != '\t' && *numList != '\n' && *numList != '\v' && *numList != '\f' && *numList != '\r')) {
+        return -1.0;
+    }
+
    /***      Apply all changes to the code above this line. DO NOT DELETE THIS COMMENT   ***/
    return value;
 }
@@ -157,42 +230,50 @@ double get1stNumberValue(const char* numList) {
 *********************************/
 char* compactNumList(char* numList) {
    /***      Apply all changes to the code below this line. DO NOT DELETE THIS COMMENT   ***/
-    char* src = numList;
-    char* dest = numList;
-    int inWord = 0;
-    while (*src != '\0') {
+    char* read = numList;  // Read pointer
+    char* write = numList; // Write pointer
+    while (*read != '\0') {
         // Skip leading spaces
-        while (*src == ' ') {
-            src++;
+        while (*read == ' ') {
+            read++;
         }
-        // Start processing a word
-        while (*src != ' ' && *src != '\0') {
-            // Skip leading zeros in the word
-            if (inWord == 0 && *src == '0') {
-                src++;
-                continue;
-            }
-            inWord = 1;
-            *dest++ = *src++;
+        if (*read == '\0') {
+            break; // If the rest of the string is spaces, we're done
         }
-        // End of a word
-        if (inWord) {
-            inWord = 0;
-            // Add a single space if the next character is not the end of the string
-            if (*src != '\0') {
-                *dest++ = ' ';
-            }
+        // Find the start of a word
+        char* word_start = read;
+        // Find the end of the word
+        while (*read != ' ' && *read != '\0') {
+            read++;
+        }
+        char* word_end = read; // End of the word
+        // Remove leading zeros from the word
+        char* ptr = word_start;
+        while (ptr < word_end && *ptr == '0') {
+            ptr++;
+        }
+        if (ptr == word_end) {
+            ptr--; // If the word is all zeros, leave a single zero
+        }
+        // Copy the word to the write position
+        while (ptr < word_end) {
+            *write++ = *ptr++;
+        }
+        // Add a space if there are more words
+        if (*read != '\0') {
+            *write++ = ' ';
         }
     }
-    // Remove trailing space if any
-    if (dest != numList && *(dest - 1) == ' ') {
-        dest--;
+    // Remove trailing space if there is one
+    if (write > numList && *(write - 1) == ' ') {
+        write--;
     }
-    // Null-terminate the resulting string
-    *dest = '\0';
+    // Null-terminate the string
+    *write = '\0';
    /***      Apply all changes to the code above this line. DO NOT DELETE THIS COMMENT   ***/
    return numList;
 }
+
 
 /*********************************
 * Problem 1.5
