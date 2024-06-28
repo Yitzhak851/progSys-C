a212981fc158fc33a1048b532fa0e2396c0e2ec7
Yitzhak851 <yitzhak851@gmail.com>
2024-06-28 14:06:45 +0300

    adding small changes at week num2

--- exercises/ex2/ex2.1.sh
+++ exercises/ex2/ex2.1.sh
@@ -1,5 +1,13 @@
+# my-solution
 cp /share/ex_data/ex2/planets.txt ./
-sort -t',' -k2n -k1r planets.txt >planets2.txt
-head -n7 planets.txt | tail -n4 | cut -f1,2 -d' ' | tr "a-z" "A-Z" | tr -d ' ' | tr ',' ':' >planets3.txt
+sort -t',' -k2n -k1r planets.txt > planets2.txt
+head -n7 planets.txt | tail -n4 | cut -f1,2 -d' ' | tr "a-z" "A-Z" | tr -d ' ' | tr ',' ':' > planets3.txt
 cut -f2 -d' ' planets.txt | sort -n | uniq -c | head -n1 | tr -d [:space:] | cut -c1 >>planets2.txt
-cat planets.txt | cut -f1 -d',' | cut -c1-2 | tr "A-Z" "a-z" | paste - -d " " planets.txt | cut -f1,4- -d" " >planets5.txt
+cat planets.txt | cut -f1 -d',' | cut -c1-2 | tr "A-Z" "a-z" | paste - -d " " planets.txt | cut -f1,4- -d" " > planets5.txt
+
+# class-solution
+cp /share/ex_data/ex2/planets.txt .
+sort -k2,2n -k1,1r planets.txt > planets2.txt
+head -n7 planets.txt | tail -n4 | cut -f1-2 -d " " | tr "a-z," "A-Z:" | tr -d " " > planets3.txt
+sort -k2,2n planets.txt | cut -f2 -d" " | uniq -c | head -n1 | tr -s " " | cut -f2 -d" " >> planets2.txt
+cut planets.txt -c1-2 | tr "A-Z" "a-z" | paste - planets.txt -d" " | cut -d" " -f1,4- > planets5.txt 
--- exercises/ex2/ex2.2.sh
+++ exercises/ex2/ex2.2.sh
@@ -1,5 +1,13 @@
+# my-solution
 cp /share/ex_data/ex2/story.txt ./
-head -n 237 story.txt | tail -n 20 | tr 'aeiouAEIOU' 'eiouaEIOUA' >story-switched-vowels.txt
+head -n 237 story.txt | tail -n 20 | tr 'aeiouAEIOU' 'eiouaEIOUA' > story-switched-vowels.txt
 cat story.txt | tr -cd "\"'" | wc -c
-cut -c3 story.txt | sort | uniq -c | sort -k1nr -k2 | head -n6 | tail -n4 | tr -s ' ' '-' >letter-stats.txt
-cat story.txt | tr [:space:] '\n' | tr [:punct:] '\n' | tr 'A-Z' 'a-z' | sort | uniq | tail -n+2 >story-words.txt
+cut -c3 story.txt | sort | uniq -c | sort -k1nr -k2 | head -n6 | tail -n4 | tr -s ' ' '-' > letter-stats.txt
+cat story.txt | tr [:space:] '\n' | tr [:punct:] '\n' | tr 'A-Z' 'a-z' | sort | uniq | tail -n+2 > story-words.txt
+
+# class-solution
+cp /share/ex_data/ex2/story.txt .
+head -n237 story.txt | tail -n20 | tr "AEIOUaeiou" "EIOUAeioua" > story-switched-vowels.txt
+cat story.txt | tr -d [:space:] | tr "'\"" "\n" | wc -l
+cat story.txt | cut -c3 | tr -s [:space:] "\n" | sort | uniq -c | sort -k1,1nr -k2,2 | tr -s " " "-" | head -n4 > letter-stats.txt
+cat story.txt | tr "[:space:][:punct:]" "\n" | tr -s "\n" | tr "A-Z" "a-z" | sort | uniq > story-words.txt
