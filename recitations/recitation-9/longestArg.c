f28d3ac8e22b9c0f459e1976c5abc5ea9425771d
Yitzhak851 <yitzhak851@gmail.com>
2024-07-03 12:13:24 +0300

    adding recitation num 9

--- recitations/recitation-9/longestArg.c
+++ recitations/recitation-9/longestArg.c
@@ -1,43 +1,44 @@
 #include <stdio.h>
 #include <string.h>
 
-char* getLongestString(char** stringList, int n);
+char *getLongestString(char **stringList, int n);
 
-int main (int argc, char** argv) {
+int main(int argc, char **argv) {
    /*** invoke concatStrings on all arguments including argv[0] ***/
-   char* longestString = getLongestString(argv, argc);  // <-- fix function invocation to ignore program name
-
+   char *longestString = getLongestString(argv, argc); // <-- fix function invocation to ignore program name
+   if (argc == 1)
+   {
+      printf("Usage: ./<prog-name> <word-1> <word-2> .... ");
+      return 1;
+   }
+   longestString = getLongestString(argv, argc-1);
    printf("Longest argument is \"%s\"\n", longestString);
-
    return 0;
 }
 
 /****************************************
-  getLongestString
-  returns a pointer to the longest string
-  in a given array of strings
-  params:
-  - stringList - array of pointres to strings
-  - n          - length of array
-  if there is more than one string with the
-  same longest length, returns the one
-  that is first on the list
+  * getLongestString - returns a pointer to the longest string
+  * in a given array of strings
+  * params:
+  *- stringList = array of pointres to strings
+  *- n = length of array
+  * if there is more than one string with the
+  * same longest length, returns the one that is first on the list
   *************************************/
-
-char* getLongestString(char** stringList, int n) {
-   char* longestString;
-
+char *getLongestString(char **stringList, int n) {
+   char *longestString;
    int i;
-   longestString = "";
-   for(i=0; i<n; i++) {
-      /*** after iteration i of the for loop,
-           longestString should hold the longest
-           string among the first i strings in stringlist
-      ***/
-      if(0) {              /*** <-- replace 0 with an appropriate expression  ***/
-                           /*** <-- add single expression here ***/
+   int maxLength = strlen(stringList[1]);
+   longestString = stringList[1];
+   for (i = 2; i < n; i++) {
+      /*** after iteration i of the for loop, longestString should hold the longest string among the first i strings in stringlist ***/
+      int currerntLength = strlen(stringList[i]);
+      if (currerntLength > maxLength) {  
+         /*** <-- replace 0 with an appropriate expression  ***/
+         longestString = stringList[i];
+         maxLength = currerntLength;
+         /*** <-- add single expression here ***/
       }
-   } //end of for
-
+   } // end of for
    return longestString;
 }
